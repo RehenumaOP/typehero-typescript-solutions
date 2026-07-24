34673a7e6f7ef8b390830a9ef2acfbe008302112
RehenumaOP <tarintuhi999@gmail.com>
2026-07-24 20:33:40 +0600

    Solve Generic Type Arguments

---
+++ Beginner/generic-type-arguments.ts
@@ -0,0 +1,22 @@
+/**
+ * Problem: Generic Type Arguments
+ * Difficulty: Beginner
+ * Platform: TypeHero
+ * Language: TypeScript
+ * Date Solved: 2026-07-24
+ */
+type GroceryItem<Name,Price,Instock> = {
+	name: Name;
+	price: Price;
+	inStock: Instock;
+}
+type CapreseSalad = 
+     GroceryItem<
+		      "Caprese Salad",
+					14.99,
+					true
+		 >;
+type GroceryStore<Name, City> = {
+	name: Name;
+	city: City;
+};
